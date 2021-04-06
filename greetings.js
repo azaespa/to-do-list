e7157c5c299f4252198df88d89fbe1de0f886056
letsdothispej <pauljamesazanes@gmail.com>
2021-04-06 17:08:02 +0800

    added clock

---
+++ greetings.js
@@ -0,0 +1,39 @@
+const inputNameForm = document.querySelector('.js-inputNameForm'),
+    input = inputNameForm.querySelector('input'),
+    greetings = document.querySelector('.js-greetings');
+
+const USER_LS = 'currentUser',
+    SHOWING_CN = 'showing';
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreetings(currentValue);
+    localStorage.setItem(USER_LS, currentValue);
+}
+
+function askForName() {
+    inputNameForm.classList.add(SHOWING_CN);
+    inputNameForm.addEventListener('submit', handleSubmit);
+}
+
+function paintGreetings(text) {
+    inputNameForm.classList.remove(SHOWING_CN);
+    greetings.classList.add(SHOWING_CN);
+    greetings.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintGreetings(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init ();
